bb0dc13bc508dd8b227e6ebcb177e4ac632a48e7
jadhavakshay33 <47377602+jadhavakshay33@users.noreply.github.com>
2020-04-02 11:14:27 +0530

    Adding Database Through Entity Framwork

---
+++ ParkingMgmt/Models/BookingSlot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ParkingMgmt.Models
+{
+    public partial class BookingSlot
+    {
+        public int Id { get; set; }
+        public string VehicleNumber { get; set; }
+        public Nullable<System.DateTime> AllocatedDate { get; set; }
+        public Nullable<System.TimeSpan> AllocatedTime { get; set; }
+        public string VehicleType { get; set; }
+    }
+}
--- ParkingSlotDataLib/Properties/AssemblyInfo.cs
+++ ParkingSlotDataLib/Properties/AssemblyInfo.cs
@@ -1,15 +1,15 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ParkingDAO")]
+[assembly: AssemblyTitle("ParkingSlotDataLib")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ParkingDAO")]
+[assembly: AssemblyProduct("ParkingSlotDataLib")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7bc9cd33-083d-4b5a-8dee-b887af24bd48")]
+[assembly: Guid("d8b8063e-7ec6-466c-96bf-a4f9f8688a0a")]
 
 // Version information for an assembly consists of the following four values:
 //
