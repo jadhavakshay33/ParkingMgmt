cfdb25d3e7d78ed970b1126a7aed4fdef00adec0
jadhavakshay33 <47377602+jadhavakshay33@users.noreply.github.com>
2020-04-05 22:24:07 +0530

    UnitTest Start

---
+++ ParkingMgmtXUnitTest/Validations/ParkingSlotMgmtValidationTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParkingMgmtXUnitTest.Validations
+{
+    public class ParkingSlotMgmtValidationTest
+    {
+    }
+}
